aaa33a3fa04015385b6ba71abbe9aaa18d307941
Eduardo Caceres <eduardo.caceresdelacalle@gmail.com>
2019-12-02 18:58:10 +0000

    Solve day 2 Bump AoCHelper version to first stable one. Update Readme.

---
+++ AoC_2019/Problem02.cs
@@ -0,0 +1,95 @@
+﻿using AoCHelper;
+using AoCHelper.Helpers;
+using FileParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC_2019
+{
+    public class Problem02 : BaseProblem
+    {
+        private const int Part2Output = 19690720;
+
+        public override void Solve_1()
+        {
+            var input = ParseInput().ToList();
+
+            input[1] = 12;
+            input[2] = 2;
+
+            //input = new[] { 1, 1, 1, 4, 99, 5, 6, 0, 99 }.ToList(); //  30,1,1,4,2,5,6,0,99
+
+            //AlterInput(ref input);
+
+            //input = new[] { 2, 4, 4, 5, 99, 0 }.ToList();   //   2,4,4,5,99,9801
+
+            //AlterInput(ref input);
+
+            AlterInput(ref input);
+
+            Console.Write($"Day 2, part 1: {input.First()}");
+        }
+
+        private void AlterInput(ref List<int> input)
+        {
+            for (int i = 0; i < input.Count;)
+            {
+                switch (input[i])
+                {
+                    case 1:
+                        input[input[i + 3]] = input[input[i + 1]] + input[input[i + 2]];
+                        break;
+
+                    case 2:
+                        input[input[i + 3]] = input[input[i + 1]] * input[input[i + 2]];
+                        break;
+
+                    case 99:
+                        return;
+
+                    default:
+                        input = new List<int> { -1 };
+                        return;
+                }
+
+                i += 4;
+            }
+        }
+
+        public override void Solve_2()
+        {
+            var originalInput = ParseInput();
+            var nounVerb = Tuple.Create(-1, -1);
+
+            foreach (var one in RangeHelpers.GenerateRange(0, 99))
+            {
+                foreach (var two in RangeHelpers.GenerateRange(0, 99))
+                {
+                    var input = originalInput.Take(originalInput.Count).ToList();
+                    input[1] = one;
+                    input[2] = two;
+
+                    AlterInput(ref input);
+
+                    if (input.First() == Part2Output)
+                    {
+                        nounVerb = Tuple.Create(one, two);
+                        break;
+                    }
+                }
+            }
+
+            int result = 100 * nounVerb.Item1 + nounVerb.Item2;
+
+            Console.Write($"Day 2, part 2: {result}");
+        }
+
+        public ICollection<int> ParseInput()
+        {
+            ParsedFile file = new ParsedFile(FilePath);
+
+            return file.NextLine().ToSingleString().Split(',').Select(int.Parse).ToList();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# AoC2019
-Advent Of Code 2019
+# Advent of Code 2019
+
+[Advent Of Code 2019](https://adventofcode.com/2019) programming puzzles.
+
+Makes use of:
+
+* [FileParser](https://github.com/eduherminio/FileParser) for parsing input files.
+
+* [AoCHelper](https://github.com/eduherminio/AoCHelper) for generalizing problem solving.
