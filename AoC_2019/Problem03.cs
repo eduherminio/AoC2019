72ea5915a506944b160614cfbb29237d65378ebb
Eduardo Caceres <eduardo.caceresdelacalle@gmail.com>
2019-12-03 19:41:58 +0000

    Solve day 3. Bump AoCHelper version to make use of its Point and Manhattan distance calculation methods.

---
+++ AoC_2019/Problem03.cs
@@ -0,0 +1,173 @@
+﻿using AoCHelper;
+using AoCHelper.Model;
+using FileParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AoCHelper.Helpers;
+
+namespace AoC_2019
+
+{
+    public class Problem03 : BaseProblem
+    {
+        public override string Solve_1()
+        {
+            var input = ParseInput().ToList();
+
+            HashSet<Point> crossPoints = new HashSet<Point>();
+            IDictionary<Point, int> allLinesPoints = new Dictionary<Point, int>();
+
+            int lineIndex = 0;
+            foreach (IEnumerable<string> line in input)
+            {
+                Point current = new Point(0, 0);
+                foreach (string ins in line)
+                {
+                    int dis = int.Parse(ins.Substring(1));
+                    char c = ins[0];
+
+                    void handlePoint(int x, int y, int lineIndex)
+                    {
+                        var newPoint = new Point(x, y);
+                        if (allLinesPoints.TryGetValue(newPoint, out int existingLineIndex))
+                        {
+                            if (existingLineIndex != lineIndex)
+                            {
+                                crossPoints.Add(newPoint);
+                            }
+                        }
+                        else
+                        {
+                            allLinesPoints.Add(newPoint, lineIndex);
+                        }
+                    }
+
+                    switch (c)
+                    {
+                        case 'U':
+                            RangeHelpers.GenerateRange(current.Y + 1, current.Y + dis).ToList()
+                                .ForEach(y => handlePoint(current.X, y, lineIndex));
+                            current.Y += dis;
+                            break;
+
+                        case 'D':
+                            RangeHelpers.GenerateRange(current.Y - dis, current.Y - 1).ToList()
+                                .ForEach(y => handlePoint(current.X, y, lineIndex));
+                            current.Y -= dis;
+                            break;
+
+                        case 'R':
+                            RangeHelpers.GenerateRange(current.X + 1, current.X + dis).ToList()
+                                .ForEach(x => handlePoint(x, current.Y, lineIndex));
+                            current.X += dis;
+                            break;
+
+                        case 'L':
+                            RangeHelpers.GenerateRange(current.X - dis, current.X - 1).ToList()
+                                .ForEach(x => handlePoint(x, current.Y, lineIndex));
+                            current.X -= dis;
+                            break;
+                    }
+                }
+                lineIndex++;
+            }
+
+            var origin = new Point(0, 0);
+            crossPoints.Remove(origin);
+
+            var closesPoint = origin.CalculateClosestManhattanPoint(crossPoints);
+
+            return origin.ManhattanDistance(closesPoint).ToString();
+        }
+
+        public override string Solve_2()
+        {
+            var input = ParseInput().ToList();
+
+            IDictionary<Point, int> crossPoints = new Dictionary<Point, int>();
+            IDictionary<Point, Tuple<int, int>> allLinesPoints = new Dictionary<Point, Tuple<int, int>>();
+
+            int lineIndex = 0;
+            foreach (IEnumerable<string> line in input)
+            {
+                Point current = new Point(0, 0);
+                int lineLength = 0;
+                foreach (string ins in line)
+                {
+                    int dis = int.Parse(ins.Substring(1));
+                    char c = ins[0];
+
+                    void handlePoint(int x, int y, int lineIndex, ref int lineLength)
+                    {
+                        lineLength++;
+                        var newPoint = new Point(x, y);
+                        if (allLinesPoints.TryGetValue(newPoint, out Tuple<int, int> existingLineIndexAndValue))
+                        {
+                            if (existingLineIndexAndValue.Item1 != lineIndex)
+                            {
+                                crossPoints.Add(newPoint, existingLineIndexAndValue.Item2 + lineLength);
+                            }
+                        }
+                        else
+                        {
+                            allLinesPoints.Add(newPoint, Tuple.Create(lineIndex, lineLength));
+                        }
+                    }
+
+                    switch (c)
+                    {
+                        case 'U':
+                            RangeHelpers.GenerateRange(current.Y + 1, current.Y + dis).ToList()
+                                .ForEach(y => handlePoint(current.X, y, lineIndex, ref lineLength));
+                            current.Y += dis;
+                            break;
+
+                        case 'D':
+                            RangeHelpers.GenerateRange(current.Y - dis, current.Y - 1).ToList()
+                                .ForEach(y => handlePoint(current.X, y, lineIndex, ref lineLength));
+                            current.Y -= dis;
+                            break;
+
+                        case 'R':
+                            RangeHelpers.GenerateRange(current.X + 1, current.X + dis).ToList()
+                                .ForEach(x => handlePoint(x, current.Y, lineIndex, ref lineLength));
+                            current.X += dis;
+                            break;
+
+                        case 'L':
+                            RangeHelpers.GenerateRange(current.X - dis, current.X - 1).ToList()
+                                .ForEach(x => handlePoint(x, current.Y, lineIndex, ref lineLength));
+                            current.X -= dis;
+                            break;
+                    }
+                }
+                lineIndex++;
+            }
+
+            var origin = new Point(0, 0);
+            crossPoints.Remove(origin);
+
+            var closestPoint = crossPoints.OrderBy(item => item.Value).First();
+
+            return closestPoint.Value.ToString();
+        }
+
+        public IEnumerable<IEnumerable<string>> ParseInput()
+        {
+            var input = new List<List<string>>();
+            var file = new ParsedFile(FilePath);
+
+            while (!file.Empty)
+            {
+                var line = file.NextLine();
+
+                input.Add(line.ToSingleString()
+                    .Split(',')
+                    .Select(s => s.ToUpperInvariant()).ToList());
+            }
+
+            return input;
+        }
+    }
+}
