6f91a4cafac62ad6e1f0c3bd9d5f61c79b383b4b
Eduardo Caceres <eduardo.caceresdelacalle@gmail.com>
2019-12-06 14:56:06 +0000

    Solve day 6 part I

---
+++ AoC_2019/Model/Node.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC_2019.Model
+{
+    public class Node : AoCHelper.Model.Node<string>
+    {
+        public ICollection<Node> Children { get; set; }
+
+        public Node(string id) : base(id)
+        {
+            Children = new List<Node>();
+        }
+
+        public Node(string id, Node child) : base(id)
+        {
+            Children = new List<Node>() { child };
+        }
+
+        public int GrandChildrenCount()
+        {
+            return Children.Count + Children.Select(child => child.GrandChildrenCount()).Sum();
+        }
+
+        public int RelationShipCount()
+        {
+            return Children.Count
+                   + Children.Select(child => child.GrandChildrenCount()).Sum()
+                   + Children.Select(child => child.RelationShipCount()).Sum();
+        }
+    }
+}
---
+++ AoC_2019/Problem06.cs
@@ -0,0 +1,65 @@
+﻿using AoCHelper;
+using FileParser;
+using System.Collections.Generic;
+using System.Linq;
+using AoC_2019.Model;
+
+namespace AoC_2019
+{
+    public class Problem06 : BaseProblem
+    {
+        public override string Solve_1()
+        {
+            var nodes = ParseInput();
+
+            var parent = nodes.Single(n => !nodes.Any(otherN => otherN.Children.Contains(n)));
+
+            var result = parent.RelationShipCount();
+
+            return result.ToString();
+        }
+
+        public override string Solve_2()
+        {
+            var input = ParseInput().ToList();
+
+            return "";
+        }
+
+        private HashSet<Node> ParseInput()
+        {
+            HashSet<Node> nodes = new HashSet<Node>();
+            HashSet<string> existingNodes = new HashSet<string>();
+
+            var inputs = new ParsedFile(FilePath).ToList<string>();
+
+            inputs.ForEach(str =>
+            {
+                IEnumerable<string> items = str.Split(')');
+
+                if (existingNodes.Add(items.Last()))
+                {
+                    nodes.Add(new Node(items.Last()));
+                }
+
+                var orbiter = nodes.Single(n => n.Id == items.Last());
+
+                if (existingNodes.Add(items.First()))
+                {
+                    nodes.Add(new Node(items.First(), orbiter));
+                }
+                else
+                {
+                    nodes.Single(n => n.Id == items.First()).Children.Add(orbiter);
+                }
+            });
+
+            if (existingNodes.Count != nodes.Count)
+            {
+                throw new SolvingException("Error parsing input");
+            }
+
+            return nodes;
+        }
+    }
+}
