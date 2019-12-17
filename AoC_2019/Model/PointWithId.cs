b1e6e681e3023b1805fd7fbf110e25ef7ce283ab
Eduardo Caceres <eduardo.caceresdelacalle@gmail.com>
2019-12-17 18:48:34 +0000

    Solve day 17 part I

---
+++ AoC_2019/Model/PointWithId.cs
@@ -0,0 +1,12 @@
+﻿namespace AoC_2019.Model
+{
+    public class PointWithId : AoCHelper.Model.Point
+    {
+        public string Id { get; set; }
+
+        public PointWithId(string id, int x, int y) : base(x, y)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ AoC_2019/Problem17.cs
@@ -0,0 +1,69 @@
+﻿using AoC_2019.IntCode;
+using AoC_2019.Model;
+using AoCHelper;
+using FileParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC_2019
+{
+    public class Problem17 : BaseProblem
+    {
+        private const string Scaffold = "#";
+        private readonly IReadOnlyCollection<string> Robot = new[] { "<", "^", ">", "v" };
+        private const string OpenSpace = ".";
+        private const string NewLine = "\n";
+        private const string TumblingRobot = "X";
+
+        public override string Solve_1()
+        {
+            var intCode = ParseInput().ToList();
+
+            var intCodeOutput = IntCodeHelpers.RunIntCodeProgram(intCode).Result;
+
+            ICollection<PointWithId> points = new List<PointWithId>();
+
+            int x = 0, y = 0;
+            foreach (long output in intCodeOutput)
+            {
+                string ch = Convert.ToChar(output).ToString();
+
+                if (ch == NewLine)
+                {
+                    ++y;
+                    x = 0;
+                    continue;
+                }
+
+                points.Add(new PointWithId(ch, x, y));
+                ++x;
+            }
+
+            var intersections = points.Where(p =>
+                    p.Id == Scaffold
+                    && points.Count(otherP =>
+                        p.ManhattanDistance(otherP) == 1 && otherP.Id == Scaffold) == 4);
+
+            static int CalculateAlignmentParameter(AoCHelper.Model.Point p) => p.X * p.Y;
+
+            int result = intersections
+                .Sum(CalculateAlignmentParameter);
+
+            return result.ToString();
+        }
+
+        public override string Solve_2()
+        {
+            return "";
+        }
+
+        private IEnumerable<long> ParseInput()
+        {
+            return new ParsedFile(FilePath)
+                .ToSingleString()
+                .Split(',')
+                .Select(long.Parse);
+        }
+    }
+}
