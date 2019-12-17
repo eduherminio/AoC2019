5c61bff2397ed5a09d720db2ecd3da9a0398581b
Eduardo Caceres <eduardo.caceresdelacalle@gmail.com>
2019-12-17 02:10:06 +0000

    Add useless optimization for day 16-2

--- AoC_2019/Problem16.cs
+++ AoC_2019/Problem16.cs
@@ -13,19 +13,67 @@ namespace AoC_2019
         public override string Solve_1()
         {
             ICollection<int> input = ParseInput().ToList();
-            const int phasesToApply = 100;
 
+            const int phasesToApply = 100;
             for (int i = 0; i < phasesToApply; ++i)
             {
-                input = ApplyPhase(input).ToList();
+                input = ApplyPhaseImproved(input).ToList();
             }
 
-            string solution = string.Join(string.Empty, input.Take(8).Select(n => n.ToString()));
-
-            return solution;
+            return string.Join(string.Empty, input.Take(8).Select(n => n.ToString()));
         }
 
-        IEnumerable<int> ApplyPhase(IEnumerable<int> inputList)
+        //public override string Solve_2()
+        //{
+        //    ICollection<int> input = ParseInput().ToList();
+
+        //    int offset = int.Parse(string.Join(string.Empty, input.Take(7).Select(n => n.ToString())));
+
+        //    List<int> realInput = new List<int>(10_000 * input.Count);
+        //    for (int i = 0; i < 10_000; ++i)
+        //    {
+        //        realInput.AddRange(input);
+        //    }
+
+        //    const int phasesToApply = 100;
+        //    List<int> rangeSolution = new List<int>();
+        //    for (int i = 0; i < phasesToApply; ++i)
+        //    {
+        //        rangeSolution = ApplyPhaseForSignalRange(realInput, offset + 1).ToList();
+        //        realInput = realInput.Take(offset).ToList();
+        //        realInput.AddRange(rangeSolution);
+        //    }
+
+        //    return string.Join(string.Empty, rangeSolution.Skip(offset).Take(8).Select(n => n.ToString()));
+        //}
+
+        /// <summary>
+        /// Naive, bruteforce implementation using improved applyphase method
+        /// </summary>
+        /// <returns></returns>
+        public override string Solve_2()
+        {
+            ICollection<int> input = ParseInput().ToList();
+
+            int offset = int.Parse(string.Join(string.Empty, input.Take(7).Select(n => n.ToString())));
+
+            List<int> realInput = new List<int>(10_000 * input.Count);
+            for (int i = 0; i < 10_000; ++i)
+            {
+                realInput.AddRange(input);
+            }
+
+            const int phasesToApply = 100;
+            for (int i = 0; i < phasesToApply; ++i)
+            {
+                realInput = ApplyPhaseImproved(realInput).ToList();
+                Console.Write($"Phase {i + 1} completed!!");
+            }
+
+            return string.Join(string.Empty, realInput.Skip(offset).Take(8).Select(n => n.ToString()));
+        }
+
+        private IEnumerable<int> ApplyPhase(IEnumerable<int> inputList)
         {
             int inputLength = inputList.Count();
 
@@ -45,15 +93,12 @@ namespace AoC_2019
                         patternIndex = ++patternIndex % _basePattern.Count;
                         patternRepetition = 0;
                     }
-                    //else
-                    //{
+
                     ++patternRepetition;
-                    //}
 
                     int input = inputList.ElementAt(inputItemIndex);
                     int multiplier = _basePattern.ElementAt(patternIndex);
                     sum += input * multiplier;
-
                 }
 
                 patternIndex = 0;
@@ -62,9 +107,99 @@ namespace AoC_2019
             }
         }
 
-        public override string Solve_2()
+        private IEnumerable<int> ApplyPhaseImproved(IEnumerable<int> inputList)
         {
-            return "";
+            int inputLength = inputList.Count();
+
+            // Index of pattern
+            int patternIndex = 0;
+
+            // repetitions of an pattern[patternIndex], starts in one to skip first item in the pattern for each 'row'
+            int patternRepetition = 1;
+            for (int inputIndex = 0; inputIndex < inputLength; ++inputIndex)    //  'row' index
+            {
+                int sum = 0;
+
+                for (int inputItemIndex = 0; inputItemIndex < inputLength; ++inputItemIndex)   // 'column' index
+                {
+                    if (patternRepetition % (inputIndex + 1) == 0)  // Note that min. number of repetition is 1, for row index 0
+                    {
+                        patternIndex = ++patternIndex % _basePattern.Count;
+                        patternRepetition = 0;
+                    }
+
+                    // Avoid multiplying and adding elements when multiplier is going to be 0
+                    if (_basePattern.ElementAt(patternIndex) == 0)
+                    {
+                        inputItemIndex += inputIndex + 1 - (inputItemIndex == 0 ? 1 : 0);
+                        if (inputItemIndex >= inputLength)
+                        {
+                            break;
+                        }
+                        patternIndex = ++patternIndex % _basePattern.Count;
+                        patternRepetition = 0;
+                    }
+
+                    ++patternRepetition;
+
+                    int input = inputList.ElementAt(inputItemIndex);
+                    if (input != 0)
+                    {
+                        int multiplier = _basePattern.ElementAt(patternIndex);
+                        sum += input * multiplier;
+                    }
+                }
+
+                patternIndex = 0;
+                patternRepetition = 1;
+                yield return Math.Abs(sum) % 10;
+            }
+        }
+
+        /// <summary>
+        /// [minRange, maxRange]
+        /// </summary>
+        /// <param name="inputList"></param>
+        /// <param name="minRange"></param>
+        /// <returns></returns>
+        private IEnumerable<int> ApplyPhaseForSignalRange(IEnumerable<int> inputList, int minRange)
+        {
+            int inputLength = inputList.Count();
+
+            // Index of pattern
+            int patternIndex = 0;
+
+            // repetitions of an pattern[patternIndex], starts in one to skip first item in the pattern for each 'row'
+            int patternRepetition = 1;
+            for (int inputIndex = minRange; inputIndex <= inputLength; ++inputIndex)    //  'row' index
+            {
+                int sum = 0;
+
+                for (int inputItemIndex = minRange; inputItemIndex < inputLength; ++inputItemIndex)   // 'column' index
+                {
+                    if (patternRepetition % (inputIndex + 1) == 0)  // Note that min. number of repetition is 1, for row index 0
+                    {
+                        patternIndex = ++patternIndex % _basePattern.Count;
+                        patternRepetition = 0;
+
+                        if (_basePattern.ElementAt(patternIndex) == 0)
+                        {
+                            inputItemIndex += inputIndex;
+                            patternIndex = ++patternIndex % _basePattern.Count;
+                        }
+                    }
+
+                    ++patternRepetition;
+
+                    int input = inputList.ElementAt(inputItemIndex);
+                    int multiplier = _basePattern.ElementAt(patternIndex);
+                    sum += input * multiplier;
+                }
+
+                patternIndex = 0;
+                patternRepetition = 1;
+                yield return Math.Abs(sum) % 10;
+            }
         }
 
         private IEnumerable<int> ParseInput()
